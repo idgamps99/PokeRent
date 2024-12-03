60b201bdb5406b814629e6789c74f26afecb9704
Armon Kaizman <armon.kaizman@gmail.com>
2024-12-03 11:38:37 +0100

    added controller action new and create. added views

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,23 @@
 class PokemonsController < ApplicationController
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    @user = current_user
+    @pokemon.user = @user
+    if @pokemon.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :ability, :pokemon_type, :price_per_day)
+  end
 end
