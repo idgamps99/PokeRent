183654b0b5dcf96c575e9bc0f7f49ed7f46d4efb
Myles Amphlett <mylesamphlett@gmail.com>
2024-12-03 17:03:31 +0000

    added better seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :pokemons
-  has_many :bookings
+  has_many :pokemons, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,41 @@
-puts "Starting seeds.."
+require 'open-uri'
+
+# Clear existing data
+User.destroy_all
+Pokemon.destroy_all
+
+# Sample Pokémon data with image URLs
 pokemon_data = [
-  { name: "Pikachu", ability: "Static", pokemon_type: "Electric", price_per_day: 12.5 },
-  { name: "Charmander", ability: "Blaze", pokemon_type: "Fire", price_per_day: 15.0 },
-  { name: "Bulbasaur", ability: "Overgrow", pokemon_type: "Grass/Poison", price_per_day: 10.0 },
-  { name: "Squirtle", ability: "Torrent", pokemon_type: "Water", price_per_day: 12.0 },
-  { name: "Eevee", ability: "Adaptability", pokemon_type: "Normal", price_per_day: 14.0 }
+  { name: 'Pikachu', ability: 'Static', pokemon_type: 'Electric', price_per_day: 10.5, image_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png' },
+  { name: 'Charmander', ability: 'Blaze', pokemon_type: 'Fire', price_per_day: 15.0, image_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png' },
+  { name: 'Bulbasaur', ability: 'Overgrow', pokemon_type: 'Grass', price_per_day: 12.0, image_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' },
+  { name: 'Squirtle', ability: 'Torrent', pokemon_type: 'Water', price_per_day: 11.0, image_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png' },
+  { name: 'Jigglypuff', ability: 'Cute Charm', pokemon_type: 'Fairy', price_per_day: 9.0, image_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png' }
 ]
 
-# Create 5 users, each with 5 Pokémon
+# Create users
 5.times do |i|
   user = User.create!(
     email: "user#{i + 1}@example.com",
-    password: "password",
-    username: "user#{i + 1}",
+    username: "User#{i + 1}",
     first_name: "FirstName#{i + 1}",
-    last_name: "LastName#{i + 1}"
+    last_name: "LastName#{i + 1}",
+    password: 'password'
   )
 
   # Assign 5 Pokémon to each user
-  pokemon_data.each do |poke|
-    user.pokemons.create!(
-      name: poke[:name],
-      ability: poke[:ability],
-      pokemon_type: poke[:pokemon_type],
-      price_per_day: poke[:price_per_day]
+  pokemon_data.each do |pokemon|
+    new_pokemon = user.pokemons.create!(
+      name: pokemon[:name],
+      ability: pokemon[:ability],
+      pokemon_type: pokemon[:pokemon_type],
+      price_per_day: pokemon[:price_per_day]
     )
+
+    # Attach the Pokémon image using Active Storage
+    file = URI.open(pokemon[:image_url])
+    new_pokemon.photo.attach(io: file, filename: "#{pokemon[:name].downcase}.png", content_type: 'image/png')
   end
 end
 
-puts "Seeded 5 users with 5 Pokémon each."
+puts "Seed data created successfully!"
