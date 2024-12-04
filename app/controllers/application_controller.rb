b44b8ea7c227bed99e08b91f8b5d4c78104ee97d
Marianne Cooper <mariannecooper@hotmail.co.uk>
2024-12-04 11:42:38 +0000

    finishes backend for the profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  # before_action :store_location
 
   protected
 
@@ -8,11 +9,18 @@ class ApplicationController < ActionController::Base
     # For additional fields in new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
     # For additional in edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar_name])
   end
 
   # Redirect user to home page after log in
   def after_sign_in_path_for(resource)
     root_path
   end
+
+  # def store_location
+  #   # Save the referring URL unless it’s a Devise path or an Ajax request
+  #   unless request.fullpath =~ /\/users\/sign_in|\/users\/sign_up|\/users\/password|\/users\/sign_out|\/users\/edit/ || request.xhr?
+  #     session[:return_to] = request.fullpath
+  #   end
+  # end
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
   validates :name, :ability, :pokemon_type, :price_per_day, presence: true
